277b7042e3d025c8343cef468829cdafb0b322c4
Ali <alierrami2024@gmail.com>
2023-06-15 16:43:22 -0700

    I am net

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #!/bin/bash
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
- int main(void)
- {
-	puts("\"Programming is like building a mutilingual puzzle");
-        return (0);
- }
+int main(void)
+{
+    puts("\""Programming is like building a multilingual puzzle);
+    return (0);
+}
