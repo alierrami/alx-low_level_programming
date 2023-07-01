fd73057dd6c3841276509382b9480103e1c56f47
Ali <alierrami2024@gmail.com>
2023-06-30 17:36:36 -0700

    Noise is a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * print_buffer - prints a buffer
  * @b: buffer
  * @size: size
  * Return: void
  */
+
 void print_buffer(char *b, int size)
 {
 	int o, j, i;
