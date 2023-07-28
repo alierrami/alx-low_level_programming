041a39af4b1c2818db2bdbb9f7c6744208d72693
ALI <alierrami2024@gmail.com>
2023-07-28 12:10:28 -0700

    Hard work

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
- *list_len - returns the number of elements in a linked list_t list
- *@h: a pointer to a list_t list
+ *list_len - returns the number of elements in a list_t list
+ *@h: pointer to a list_t list to print
  *Return: number of elements in a list_t list
  */
 size_t list_len(const list_t *h)
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,31 +1,26 @@
 #include "lists.h"
-
 /**
- *add_node - function that adds a new node at the beginning of a list_t list.
- *@head: pointer to a pointer
- *@str : pointer to a string.
- *Return: list_t
+ *add_node - adds a new node at the beginning of a list_t list
+ *@head: pointer to pointer to lis_t list
+ *@str : string to add to the node
+ *Return: the address of the new element, or NULL if it failed
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *newNode;
+	list_t *newNode;
 
-newNode = malloc(sizeof(list_t));
-
-if (newNode == NULL)
-{
-return (NULL);
+	newNode = malloc(sizeof(list_t));
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		newNode->str = strdup(str);
+		newNode->len = strlen(str);
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
 }
 
-else
-{
-newNode->str = strdup(str);
-
-newNode->len = strlen(str);
-newNode->next = *head;
-*head = newNode;
-
-return (newNode);
-}
-}
