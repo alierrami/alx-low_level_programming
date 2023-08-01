6801a4256e7e8f74e5459fac254945f1e1671eef
ALI <alierrami2024@gmail.com>
2023-08-01 09:11:18 -0700

    Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,19 +2,20 @@
 /**
  * pop_listint - deletes the head node of a listint_t list
  * @head: double pointer to the listint_t list
- * Return: the head node's data
+ * Return: the head node's data to be deleted
  */
 int pop_listint(listint_t **head)
 {
 	listint_t *temp = *head;
+	int num;
 
-	if (*head == NULL)
+	if (head == NULL || (*head) == NULL)
 		return (0);
-	while (*head)
-	{
-		*head = (*head)->next;
-		free(temp);
-	}
-	return ((*head)->n);
+
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+	return (num);
 }
 
