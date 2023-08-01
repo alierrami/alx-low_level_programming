6b254fbeabc65ba280ceeadd5673f2fccc0c3455
ALI <alierrami2024@gmail.com>
2023-08-01 05:15:51 -0700

    List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * listint_len - returns the number of elements in a listint_t list
+ * @h: a pointer to the listint_t list
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t n;
+
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
+
