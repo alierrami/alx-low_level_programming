4a8769b4ab2a1d15fe13d4f63da37d608bef144d
ALI <alierrami2024@gmail.com>
2023-08-01 17:08:36 -0700

    Add node at the end

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,19 +10,14 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int count = 0;
 	listint_t *current = head;
 
-	while (current != NULL)
+	while (current && count < index)
 	{
-		if (count == index)
-		{
-			return (current);
-		}
 		count++;
 		current = current->next;
 	}
-
-	if (head == NULL)
-		return (NULL);
+	return (current ? current : NULL);
 }
+
 
 
 
