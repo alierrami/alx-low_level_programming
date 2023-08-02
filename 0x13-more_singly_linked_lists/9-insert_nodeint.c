f2c81c03ad7b7dc49f25aa6de02898ff096bd67d
ALI <alierrami2024@gmail.com>
2023-08-01 18:21:40 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	for (int i = 0; i < idx && temp != NULL; i++)
+	for (int i = 0; i < idx && temp; i++)
 	{
 		if (i == idx - 1)
 		{
