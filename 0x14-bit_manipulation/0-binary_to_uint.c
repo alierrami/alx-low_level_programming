ba3698199ab5ea0d0128b670b660f57aa8b338a2
ALI <alierrami2024@gmail.com>
2023-08-04 11:12:01 -0700

    Binary number to ui

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,30 @@
 #include "main.h"
-
 /**
- *binary_to_uint - function that convert binary to ui
- *@b : pointer to the string
- *
- *Return:converted number, or 0
+ *binary_to_uint - converts a binary number  to an ui
+ *@b: a pointer to the string of 0 and 1 chars
+ *Return: converted number or 0
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-int i;
-unsigned int num = 0;
+	int i;
+	unsigned int num = 0;
 
-if (b == NULL)
-{
-return (0);
-}
+	if (b == NULL)
+	{
+		return (0);
+	}
 
-for (i = 0; b[i] != '\0'; i++)
-{
-if (b[i] != '0' && b[i] != '1')
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
 
-return (0);
-}
-
-for (i = 0; b[i] != '\0'; i++)
-{
-num <<= 1;
-
-if (b[i] == '1')
-num += 1;
-
-}
-
-return (num);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
+	}
+	return (num);
 }
