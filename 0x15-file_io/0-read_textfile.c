b8084183946ea193a5fde03f15bf4aa5f5f02c26
ALI <alierrami2024@gmail.com>
2023-08-08 16:11:18 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,18 +1,10 @@
-/*
- * File: 0-read_textfile.c
- * Auth: Dr Marcus.
- */
 #include "main.h"
-#include <stdlib.h>
-
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
- *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
+ * read_textfile - reads a text file and prints it to the POSIX stdout
+ * @filename: a pointer to the name of the file
+ * @letters: the number of letters the function should read and print
+ * Return: 0 if the function fails or filename is NULL
+ *         the actual number of bytes the function can read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
