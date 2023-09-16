d67b60ffdcff29bdc69b173792e486877d14d4ca
ALI <alierrami2024@gmail.com>
2023-09-16 06:26:55 -0700

    Double linked lists

--- 0x17-doubly_linked_list/lists.h
+++ 0x17-doubly_linked_list/lists.h
@@ -1,6 +1,8 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <stddef.h>
 /**
  * struct dlistint_s - doubly linked list
@@ -28,4 +30,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif/*LIST_H*/
+#endif
