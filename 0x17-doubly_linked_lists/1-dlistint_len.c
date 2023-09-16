9c8ebbe1175d2362d0d99d60d35cc19d97d2ade8
ALI <alierrami2024@gmail.com>
2023-09-16 14:06:19 -0700

    Double linked lists

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * dlistint_len - counts the number of nodes in the linked list
+ * @h: a pointer to the beginning of the linked list
+ *
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while( h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
+
