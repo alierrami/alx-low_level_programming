80a63630d8f779349ad59738768698899d36e9c1
ALI <alierrami2024@gmail.com>
2023-09-18 14:27:01 -0700

    Dynamic libraries

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts - function that prints a string
+ *@str: The parametre string passed to function
+ *
+ *Return: always return 0.
+ */
+void _puts(char *str)
+{
+printf("%s\n", str);
+}
+
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  * _abs - Computes the absolute value of an integer.
+  * @n: The number to be computed.
+  *
+  * Return: Absolute value of number or zero
+  */
+int _abs(int n)
+{
+int r = abs(n);
+return (r);
+}
+
