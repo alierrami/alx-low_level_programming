a0df147f3806e1c42e4889f2e4d9f9af1d437938
ALI <alierrami2024@gmail.com>
2023-09-19 08:04:57 -0700

    Dynamic libraries

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit
+ * @c: the integer to be checked
+ * Return: 1 if c is a digit or 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
