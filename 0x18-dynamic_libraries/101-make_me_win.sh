376ddd04214bef67a8f2ddbc956f0d5a0b2cf708
ALI <alierrami2024@gmail.com>
2023-09-20 16:21:44 -0700

    Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -P /tmp https://github.com/alierrami/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+wget -P /tmp https://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@github.com/alierrami/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
 
